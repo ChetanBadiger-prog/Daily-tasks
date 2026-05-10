75206580139cae1cba99d5f2469df118649f248b
Chethan Badiger <chetandvg2004@gmail.com>
2026-05-10 10:22:52 +0530

    calculator

---
+++ calculator.js
@@ -0,0 +1,23 @@
+
+// // function handleoperator(op){
+// //     console.log(op);
+    
+// // }
+
+// let exp = "";
+
+// function handleoperator(op) {
+//     if (op === 'C') return console.log("Cleared!"), exp = "";
+
+//     if (op === '=') {
+//         try {
+//             exp = eval(exp).toString();
+//             return console.log("Result:", exp);
+//         } catch {
+//             return console.log("Invalid Expression!"), exp = "";
+//         }
+//     }
+
+//     console.log("Current:", exp += op);
+// }
+
